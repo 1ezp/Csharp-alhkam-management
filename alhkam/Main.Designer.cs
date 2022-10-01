b9f95ae01a12c69cf13669fd29b41a6e62b26963
hikmet <hekmt111@gmail.com>
2022-10-01 12:13:42 +0300

    add panels

--- alhkam/Main.Designer.cs
+++ alhkam/Main.Designer.cs
@@ -29,21 +29,61 @@ namespace alhkam
         /// </summary>
         private void InitializeComponent()
         {
+            this.pn_topbar = new System.Windows.Forms.Panel();
+            this.pn_navbar = new System.Windows.Forms.Panel();
+            this.pn_cont = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pn_topbar
+            // 
+            this.pn_topbar.BackColor = System.Drawing.Color.Gray;
+            this.pn_topbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pn_topbar.Location = new System.Drawing.Point(0, 0);
+            this.pn_topbar.Name = "pn_topbar";
+            this.pn_topbar.Size = new System.Drawing.Size(1012, 52);
+            this.pn_topbar.TabIndex = 0;
+            // 
+            // pn_navbar
+            // 
+            this.pn_navbar.BackColor = System.Drawing.Color.Gray;
+            this.pn_navbar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pn_navbar.Location = new System.Drawing.Point(803, 52);
+            this.pn_navbar.Name = "pn_navbar";
+            this.pn_navbar.Size = new System.Drawing.Size(209, 465);
+            this.pn_navbar.TabIndex = 1;
+            // 
+            // pn_cont
+            // 
+            this.pn_cont.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.pn_cont.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pn_cont.Location = new System.Drawing.Point(0, 52);
+            this.pn_cont.Name = "pn_cont";
+            this.pn_cont.Size = new System.Drawing.Size(803, 465);
+            this.pn_cont.TabIndex = 2;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 429);
+            this.ClientSize = new System.Drawing.Size(1012, 517);
+            this.Controls.Add(this.pn_cont);
+            this.Controls.Add(this.pn_navbar);
+            this.Controls.Add(this.pn_topbar);
+            this.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "Alhkam Trading";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pn_topbar;
+        private System.Windows.Forms.Panel pn_navbar;
+        private System.Windows.Forms.Panel pn_cont;
     }
 }
 
